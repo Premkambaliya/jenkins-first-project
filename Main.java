b3bac601aeebc782c0586ce0abf974179500e7f8
Kambaliya Prem <prem.kambaliya.cg@gmail.com>
2026-04-24 14:31:09 +0530

    add change

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Z, World!");
+        System.out.println("Z, World! hi sirrr");
         System.out.println(", World!");
     }
 }
